b9871aa9bae58d842e1d5c0d58d51c720d92ca76
stratigr8217 <stratigr8217@student.faytechcc.edu>
2024-09-16 11:50:38 -0400

    Add files via upload

---
+++ P1HW1_Stratigos_Richard.py
@@ -0,0 +1,17 @@
+# Richard Stratigos
+# 09/16/2024
+# P1HW1
+# Assignment tests student's knowledge of how to write code that collects information from user, processes information collected and display results to user.
+
+print("----Calculating Exponents----")
+
+base_value = int(input("Enter an integer as the base value: "))
+exponent_value = int(input("Enter an integer as the exponent: "))
+
+# Convert base_value to integer
+
+base_value = int(base_value)
+exponent_value = int(exponent_value)
+
+print(base_value , "raised to the power of" , exponent_value, "is" , base_value**exponent_value + "!!")
+print("----Addition and Subtraction----)
