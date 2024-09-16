7557cd696cb98a586da54405d13e020166492e63
stratigr8217 <stratigr8217@student.faytechcc.edu>
2024-09-16 11:19:33 -0400

    Add files via upload

---
+++ P1LAB1_Stratigos_Richard.py
@@ -0,0 +1,13 @@
+#Richard Stratigos
+#09/16/2024
+#P1LAB1
+#Get input from user and display output
+
+#Get first name from user
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+#Display information to user
+
+print("Hello, " , first_name , last_name + "!" , "Welcome to CTI-110")
